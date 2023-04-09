7c3483b4fe3169178df8ea145b812370f077c1ae
Christian <christian.nguyen6@hotmail.com>
2023-04-09 18:31:45 +1000

    My projects done, looking to add contact me in a navbar and add more animations - possibly framer

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,43 @@
+import Head from "next/head";
 import Image from "next/image";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import YoutubeIcon from "@mui/icons-material/YouTube";
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+import { Chip } from '@mui/material';
+
 
 export default function Home () {
+  const projectCard = {
+    background: "#222a3b",
+    color: "white"
+  }
+
+  const chipStyle = {
+    background: "#516ACF",
+    marginLeft: "5px",
+    marginRight: "5px"
+  }
+
+  const chipContainerStyle = {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    paddingRight: "10px",
+    paddingBottom: "10px",
+  }
+
   return (
     <>
+      <Head>
+        <title>Christian&apos;s Portfolio</title>
+      </Head>
       <div
         style={{
           display: "flex",
@@ -26,11 +56,11 @@ export default function Home () {
           style={{ borderRadius: "50%" }}
         />
 
-        <h1 style={{ textAlign: "center" }}>
+        <h1 style={{ fontSize: "50pt", textAlign: "center" }}>
           Hi, I&apos;m Christian
         </h1>
 
-        <h2 style={{ textAlign: "center", marginTop: "-2vh" }}>
+        <h2 style={{ textAlign: "center", marginTop: "-4vh" }}>
           UNSW Computer Science
         </h2>
 
@@ -102,7 +132,14 @@ export default function Home () {
         </h1>
 
         <p style={{ textAlign: "center" }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu libero et sem iaculis ullamcorper id in ipsum. Proin bibendum, lectus non eleifend elementum, nisi tellus ornare lectus, eget porta ligula tellus quis odio. Morbi convallis justo vehicula egestas rutrum. Integer venenatis, ex eu vestibulum fringilla, nunc metus ullamcorper diam, vel malesuada justo nulla non urna. Pellentesque magna leo, rhoncus ut dui nec, vehicula vehicula purus. Praesent porttitor finibus metus, lacinia sodales lorem efficitur quis.
+          I&apos;m an aspiring software engineer in my final year looking to find ways to continually
+          challenge myself while having fun. My dream is to work on real-world problems that
+          can have a lasting positive impact in the world. So far I&apos;ve only completed a couple projects but I&apos;m
+          looking to expand my horizons and build a full-stack app for movie recommendations.
+        </p>
+
+        <p style={{ textAlign: "center" }}>
+          (I have a dog too 😃)
         </p>
       </div>
 
@@ -120,10 +157,137 @@ export default function Home () {
           My Projects
         </h1>
 
-        <p style={{ textAlign: "center" }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu libero et sem iaculis ullamcorper id in ipsum. Proin bibendum, lectus non eleifend elementum, nisi tellus ornare lectus, eget porta ligula tellus quis odio. Morbi convallis justo vehicula egestas rutrum. Integer venenatis, ex eu vestibulum fringilla, nunc metus ullamcorper diam, vel malesuada justo nulla non urna. Pellentesque magna leo, rhoncus ut dui nec, vehicula vehicula purus. Praesent porttitor finibus metus, lacinia sodales lorem efficitur quis.
-        </p>
-      </div>
+        <div
+          style={{
+            display: "grid",
+            gridGap: "30px",
+            justifyContent: "center",
+            flexDirection: "column"
+          }}
+        >
+          <Card style={projectCard} sx={{ width: "70vw", height: "40vh", position: "relative" }}>
+            <CardMedia
+              component="img"
+              height="250px"
+              image="/images/airbrb.jpeg"
+            />
+            <CardContent>
+              <Typography gutterbottom variant="h5" component="div">
+                Airbrb
+              </Typography>
+              <Typography variant="body1" color="#d2d2d4">
+                A clone of the popular renting platform Airbnb which had features including:
+                <br />
+                - Logins and signups
+                <br />
+                - Viewing and making Listings
+                <br />
+                - Making and cancelling Bookings
+                <br />
+                - Searching through listings with filters
+              </Typography>
+
+              <div style={chipContainerStyle}>
+                <Chip
+                  label="React"
+                  style={chipStyle}
+                  color="primary"
+                />
+                <Chip
+                  label="Material UI"
+                  style={chipStyle}
+                  color="primary"
+                />
+                <Chip
+                  label="HTML/CSS/JS"
+                  style={chipStyle}
+                  color="primary"
+                />
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card style={projectCard} sx={{ width: "70vw", height: "40vh", position: "relative" }}>
+            <CardMedia
+              component="img"
+              height="250px"
+              image="/images/flockr.jpeg"
+            />
+            <CardContent>
+              <Typography gutterbottom variant="h5" component="div">
+                Flockr
+              </Typography>
+              <Typography variant="body1" color="#d2d2d4">
+                A clone of an online collaboration platform called Flock. Implemented features such as:
+                <br />
+                - Logins and signups WITH authentication
+                <br />
+                - Joining and creating channels
+                <br />
+                - Role management of users in the channel
+                <br />
+                - Sending and removing messages, including additional features such as standup and pinning messages
+              </Typography>
+              <div style={chipContainerStyle}>
+                <Chip
+                  label="Python"
+                  style={chipStyle}
+                  color="primary"
+                />
+                <Chip
+                  label="Flask"
+                  style={chipStyle}
+                  color="primary"
+                />
+                <Chip
+                  label="REST APIs"
+                  style={chipStyle}
+                  color="primary"
+                />
+                <Chip
+                  label="pytest"
+                  style={chipStyle}
+                  color="primary"
+                />
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card style={projectCard} sx={{ width: "70vw", height: "40vh", position: "relative" }}>
+            <CardMedia
+              component="img"
+              height="250px"
+              image="/images/loopmania.jpeg"
+            />
+            <CardContent>
+              <Typography gutterbottom variant="h5" component="div">
+                Loop Mania
+              </Typography>
+              <Typography variant="body1" color="#d2d2d4">
+                A clone of an endless RPG game similar to the likes of Loop Hero.
+                Heavy focus on implementing various design patterns and following OOP principles throughout the project. Also wrote JUnit tests to ensure the game performed as expected. Our hero was Drake.
+              </Typography>
+              <div style={chipContainerStyle}>
+                <Chip
+                  label="Java"
+                  style={chipStyle}
+                  color="primary"
+                />
+                <Chip
+                  label="OOP"
+                  style={chipStyle}
+                  color="primary"
+                />
+                <Chip
+                  label="JUnit Testing"
+                  style={chipStyle}
+                  color="primary"
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div >
     </>
   )
 }
