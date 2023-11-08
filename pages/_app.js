fe94f5d01ca8fe061fb6ed9ee142767a3ee1d65a
scoogii <christian.nguyen6@outlook.com>
2023-11-08 14:46:57 +1100

    changed colours and removed some elements

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { CssBaseline } from "@mui/material";
 
-export default function App ({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,29 @@ import Head from "next/head";
 import Image from "next/image";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import YoutubeIcon from "@mui/icons-material/YouTube";
-import EmailIcon from "@mui/icons-material/Email"
+import EmailIcon from "@mui/icons-material/Email";
 
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Tooltip from '@mui/material/Tooltip';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
 
-import { Chip } from '@mui/material';
-import { Button } from '@mantine/core';
+import { Chip } from "@mui/material";
+import { Button } from "@mantine/core";
 import { Fade } from "react-awesome-reveal";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-
-export default function Home () {
-
+export default function Home() {
   const projectCard = {
-    background: "#1c2230"
-  }
+    background: "#1c2230",
+  };
 
   const chipStyle = {
     background: "#516ACF",
     marginLeft: "5px",
-    marginRight: "5px"
-  }
+    marginRight: "5px",
+  };
 
   const chipContainerStyle = {
     position: "absolute",
@@ -36,7 +32,7 @@ export default function Home () {
     right: 0,
     paddingRight: "10px",
     paddingBottom: "10px",
-  }
+  };
 
   return (
     <>
@@ -44,24 +40,7 @@ export default function Home () {
         <title>Christian&apos;s Portfolio</title>
       </Head>
 
-      <ul className="background">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-
-      <Fade
-        direction="up"
-        duration="1100"
-        triggerOnce="true"
-      >
+      <Fade direction="up" duration="1100" triggerOnce="true">
         <div
           style={{
             display: "flex",
@@ -69,6 +48,7 @@ export default function Home () {
             alignItems: "center",
             justifyContent: "center",
             height: "100vh",
+            background: "#141a27",
           }}
         >
           <motion.div
@@ -77,7 +57,7 @@ export default function Home () {
             transition={{
               duration: 0.8,
               delay: 0.5,
-              ease: [0, 0.71, 0.2, 1.01]
+              ease: [0, 0.71, 0.2, 1.01],
             }}
           >
             <Image
@@ -97,7 +77,7 @@ export default function Home () {
           <h2
             style={{ textAlign: "center", marginTop: "-50px" }}
             sx={{
-              marginTop: { md: -200, lg: -200 }
+              marginTop: { md: -200, lg: -200 },
             }}
           >
             UNSW Computer Science
@@ -113,9 +93,8 @@ export default function Home () {
             <Button
               size="md"
               className="socialButton"
-              variant="gradient"
               style={{
-                marginRight: "15px"
+                marginRight: "15px",
               }}
               component="a"
               href="https://github.com/scoogii"
@@ -127,10 +106,9 @@ export default function Home () {
             <Button
               size="md"
               className="socialButton"
-              variant="gradient"
               style={{
                 marginLeft: "15px",
-                marginRight: "15px"
+                marginRight: "15px",
               }}
               component="a"
               href="https://linkedin.com/in/christianngu"
@@ -139,11 +117,9 @@ export default function Home () {
               <LinkedInIcon />
             </Button>
 
-
             <Button
               size="md"
               className="socialButton"
-              variant="gradient"
               style={{
                 marginLeft: "15px",
                 marginRight: "15px",
@@ -153,83 +129,31 @@ export default function Home () {
             >
               <EmailIcon />
             </Button>
-
-            <Button
-              size="md"
-              className="socialButton"
-              variant="gradient"
-              style={{
-                marginLeft: "15px",
-              }}
-              component="a"
-              href="https://www.youtube.com/watch?v=xfeys7Jfnx8&ab_channel=nigahiga"
-              target="_blank"
-            >
-              <YoutubeIcon />
-            </Button>
           </div>
+        </div>
+      </Fade>
 
-        </div >
-      </Fade >
-
-      <hr
-        style={{
-          height: "2px",
-          border: "none",
-          borderRadius: "6px",
-          background: "linear-gradient(to right, #6AADCF 7%, #516ACF 66%"
-        }}
-      />
-
-      <div>
-        <Fade
-          direction="up"
-          duration="1100"
-          triggerOnce="true"
-        >
-          <h1 style={{ textAlign: "center" }}>
-            About Me
-          </h1>
+      <div style={{ background: "#141a27", padding: "10px" }}>
+        <Fade direction="up" duration="1100" triggerOnce="true">
+          <h1 style={{ textAlign: "center" }}>About Me</h1>
         </Fade>
 
         <p style={{ textAlign: "center" }}>
-          I&apos;m an aspiring software engineer in my final year who loves to find ways to continually
-          challenge myself. My dream is to work on real-world problems that
-          can have a lasting positive impact in the world. I&apos;m becoming a big fan of watches so if anyone wants
-          to chat about that, feel free to contact me anytime!
+          I&apos;m an aspiring software engineer in my final year who loves to
+          find ways to continually challenge myself. My dream is to work on
+          real-world problems that can have a lasting positive impact in the
+          world. I&apos;ve been getting into bouldering recently so if anyone
+          wants to chat about that, feel free to contact me anytime!
         </p>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Tooltip title="meow">
-            <Image
-              priority
-              className="gloriousOllie"
-              src="/images/ollitoes.jpg"
-              height={100}
-              width={100}
-              alt=""
-            />
-          </Tooltip>
-        </div>
       </div>
 
-      <hr
+      <div
         style={{
-          height: "3px",
-          border: "none",
-          borderRadius: "6px",
-          background: "linear-gradient(to right, #6AADCF 7%, #516ACF 66%"
+          background: "#141a27",
         }}
-      />
-
-      <div>
-        <Fade
-          direction="up"
-          duration="1100"
-          triggerOnce="true"
-        >
-          <h1 style={{ textAlign: "center" }}>
-            My Projects
-          </h1>
+      >
+        <Fade direction="up" duration="1100" triggerOnce="true">
+          <h1 style={{ textAlign: "center" }}>My Projects</h1>
         </Fade>
 
         <div
@@ -238,7 +162,8 @@ export default function Home () {
             gridGap: "50px",
             justifyContent: "center",
             alignItems: "center",
-            flexDirection: "column"
+            flexDirection: "column",
+            background: "#141a27",
           }}
         >
           <Card
@@ -247,7 +172,7 @@ export default function Home () {
             sx={{
               width: { xs: "80vw", sm: "55vw" },
               height: { xs: 600, sm: "85vh", md: 550, lg: 550 },
-              position: "relative"
+              position: "relative",
             }}
           >
             <CardMedia
@@ -256,7 +181,12 @@ export default function Home () {
               image="/images/airbrb.jpeg"
             />
             <CardContent>
-              <Typography gutterbottom="true" variant="h5" component="div" color="white">
+              <Typography
+                gutterbottom="true"
+                variant="h5"
+                component="div"
+                color="white"
+              >
                 Airbrb
               </Typography>
               <Typography
@@ -266,33 +196,21 @@ export default function Home () {
                   fontSize: { xs: 15, sm: 16, md: 16, lg: 18 },
                 }}
               >
-                A clone of the popular renting platform Airbnb which had features including:
+                A clone of the popular renting platform Airbnb which had
+                features including:
                 <br />
                 - Logins and signups
                 <br />
                 - Viewing and making Listings
                 <br />
                 - Making and cancelling Bookings
-                <br />
-                - Searching through listings with filters
+                <br />- Searching through listings with filters
               </Typography>
 
               <div style={chipContainerStyle}>
-                <Chip
-                  label="React"
-                  style={chipStyle}
-                  color="primary"
-                />
-                <Chip
-                  label="Material UI"
-                  style={chipStyle}
-                  color="primary"
-                />
-                <Chip
-                  label="HTML/CSS/JS"
-                  style={chipStyle}
-                  color="primary"
-                />
+                <Chip label="React" style={chipStyle} color="primary" />
+                <Chip label="Material UI" style={chipStyle} color="primary" />
+                <Chip label="HTML/CSS/JS" style={chipStyle} color="primary" />
               </div>
             </CardContent>
           </Card>
@@ -303,7 +221,7 @@ export default function Home () {
             sx={{
               width: { xs: "80vw", sm: "55vw" },
               height: { xs: 600, sm: "85vh", md: 550, lg: 550 },
-              position: "relative"
+              position: "relative",
             }}
           >
             <CardMedia
@@ -312,7 +230,12 @@ export default function Home () {
               image="/images/flocks.gif"
             />
             <CardContent>
-              <Typography gutterbottom="true" variant="h5" component="div" color="white">
+              <Typography
+                gutterbottom="true"
+                variant="h5"
+                component="div"
+                color="white"
+              >
                 Flockr
               </Typography>
               <Typography
@@ -322,37 +245,22 @@ export default function Home () {
                   fontSize: { xs: 15, sm: 16, md: 16, lg: 18 },
                 }}
               >
-                A clone of an online collaboration platform called Flock. Implemented features such as:
+                A clone of an online collaboration platform called Flock.
+                Implemented features such as:
                 <br />
                 - Logins and signups WITH authentication
                 <br />
                 - Joining and creating channels
                 <br />
                 - Role management of users in the channel
-                <br />
-                - Sending and removing messages, including additional features such as standup and pinning messages
+                <br />- Sending and removing messages, including additional
+                features such as standup and pinning messages
               </Typography>
               <div style={chipContainerStyle}>
-                <Chip
-                  label="Python"
-                  style={chipStyle}
-                  color="primary"
-                />
-                <Chip
-                  label="Flask"
-                  style={chipStyle}
-                  color="primary"
-                />
-                <Chip
-                  label="REST APIs"
-                  style={chipStyle}
-                  color="primary"
-                />
-                <Chip
-                  label="pytest"
-                  style={chipStyle}
-                  color="primary"
-                />
+                <Chip label="Python" style={chipStyle} color="primary" />
+                <Chip label="Flask" style={chipStyle} color="primary" />
+                <Chip label="REST APIs" style={chipStyle} color="primary" />
+                <Chip label="pytest" style={chipStyle} color="primary" />
               </div>
             </CardContent>
           </Card>
@@ -363,7 +271,7 @@ export default function Home () {
             sx={{
               width: { xs: "80vw", sm: "55vw" },
               height: { xs: 600, sm: "85vh", md: 550, lg: 550 },
-              position: "relative"
+              position: "relative",
             }}
           >
             <CardMedia
@@ -372,7 +280,12 @@ export default function Home () {
               image="/images/loopest.gif"
             />
             <CardContent>
-              <Typography gutterbottom="true" variant="h5" component="div" color="white">
+              <Typography
+                gutterbottom="true"
+                variant="h5"
+                component="div"
+                color="white"
+              >
                 Loop Mania
               </Typography>
               <Typography
@@ -382,30 +295,21 @@ export default function Home () {
                   fontSize: { xs: 15, sm: 16, md: 16, lg: 18 },
                 }}
               >
-                A clone of an endless RPG game similar to the likes of Loop Hero.
-                Heavy focus on implementing various design patterns and following OOP principles throughout the project. Also wrote JUnit tests to ensure the game performed as expected. Our hero was Drake.
+                A clone of an endless RPG game similar to the likes of Loop
+                Hero. Heavy focus on implementing various design patterns and
+                following OOP principles throughout the project. Also wrote
+                JUnit tests to ensure the game performed as expected. Our hero
+                was Drake.
               </Typography>
               <div style={chipContainerStyle}>
-                <Chip
-                  label="Java"
-                  style={chipStyle}
-                  color="primary"
-                />
-                <Chip
-                  label="OOP"
-                  style={chipStyle}
-                  color="primary"
-                />
-                <Chip
-                  label="JUnit Testing"
-                  style={chipStyle}
-                  color="primary"
-                />
+                <Chip label="Java" style={chipStyle} color="primary" />
+                <Chip label="OOP" style={chipStyle} color="primary" />
+                <Chip label="JUnit Testing" style={chipStyle} color="primary" />
               </div>
             </CardContent>
           </Card>
         </div>
-      </div >
+      </div>
     </>
-  )
+  );
 }
